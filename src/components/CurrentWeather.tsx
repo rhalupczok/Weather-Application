44e7837cc951be6553c356640cc62815244dae4c
rhalupczok <r.halupczok@gmail.com>
2023-10-08 11:47:55 +0200

    CurrentWeather componenet optimisation - functions for repeated expressions

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -8,22 +8,38 @@ const CurrentWeather: React.FC<Props> = ({ weatherDataHandle }) => {
     const [weatherData, setWeatherData] =
         React.useState<MainWeatherData | null>(null);
 
-    const time: number = new Date().valueOf();
-    const x: number =
-        ((Math.round(time / 1000) - weatherDataHandle.sys.sunrise) * 100) /
-        (weatherDataHandle.sys.sunset - weatherDataHandle.sys.sunrise);
-
-    let angle = 0;
-    let display = "flex";
-
-    if (0 < x && x <= 100) {
-        angle = (180 * x) / 100;
-        display = "flex";
-    } else {
-        angle = 0;
-        display = "none";
+    function sunWalk() {
+        //sunwalk effect - func returns style of div with sun symbol - angle depends on current time
+        const time: number = new Date().valueOf(); //current time
+        const x: number = //percentage value of current time counted since sunrise to sunset
+            ((Math.round(time / 1000) - weatherDataHandle.sys.sunrise) * 100) /
+            (weatherDataHandle.sys.sunset - weatherDataHandle.sys.sunrise);
+        let sunWalkProperties = {
+            angle: 0,
+            display: "flex",
+        };
+        if (0 < x && x <= 100) {
+            //if current time is betwen sunrise and sunset (daytime) then display sun and return propper angle
+            sunWalkProperties.angle = (180 * x) / 100;
+            sunWalkProperties.display = "flex";
+        } else {
+            sunWalkProperties.angle = 0;
+            sunWalkProperties.display = "none";
+        }
+        return {
+            transform: `rotate(${sunWalkProperties.angle}deg)`,
+            display: `${sunWalkProperties.display}`,
+        };
     }
 
+    function clockFormatter(time: Date) {
+        return (
+            time.getHours() +
+            ":" +
+            (time.getMinutes() < 10 ? "0" : "") +
+            time.getMinutes()
+        );
+    }
     React.useEffect(() => {
         if (weatherDataHandle) {
             const sunRise = new Date(weatherDataHandle.sys.sunrise * 1000);
@@ -41,25 +57,13 @@ const CurrentWeather: React.FC<Props> = ({ weatherDataHandle }) => {
                 tempMax: weatherDataHandle.main.temp_max.toPrecision(2),
                 tempMin: weatherDataHandle.main.temp_min.toPrecision(2),
                 desc: weatherDataHandle.weather[0].description,
-                sunRise:
-                    sunRise.getHours() +
-                    ":" +
-                    (sunRise.getMinutes() < 10 ? "0" : "") +
-                    sunRise.getMinutes(),
-                sunSet:
-                    sunSet.getHours() +
-                    ":" +
-                    (sunSet.getMinutes() < 10 ? "0" : "") +
-                    sunSet.getMinutes(),
+                sunRise: clockFormatter(sunRise),
+                sunSet: clockFormatter(sunSet),
                 windSpeed: weatherDataHandle.wind.speed + "km/h",
                 humidity: weatherDataHandle.main.humidity + "%",
                 pressure: weatherDataHandle.main.pressure + "HPa",
                 cloudsAmount: weatherDataHandle.clouds.all + "%",
-                currentTime:
-                    currentTime.getHours() +
-                    ":" +
-                    (currentTime.getMinutes() < 10 ? "0" : "") +
-                    currentTime.getMinutes(),
+                currentTime: clockFormatter(currentTime),
                 currentDay: `${currentTime.getDate()}-${currentTime.getMonth()}-${currentTime.getFullYear()}`,
             }));
         }
@@ -136,13 +140,7 @@ const CurrentWeather: React.FC<Props> = ({ weatherDataHandle }) => {
                     {weatherData.currentDay}
                 </div>
                 <div className="current-weather--semicircle"></div>
-                <div
-                    className="current-weather--sun-rotate"
-                    style={{
-                        transform: `rotate(${angle}deg)`,
-                        display: `${display}`,
-                    }}
-                >
+                <div className="current-weather--sun-rotate" style={sunWalk()}>
                     <div className="current-weather--sun">
                         <img src={require("../images/sun.png")} alt="sun" />
                     </div>
